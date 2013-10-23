8d06732bae747cf53e2524e3e49e10f0d0f9f25b
Dimitri Sabadie <dimitri.sabadie@me.com>
2013-10-23 14:42:27 +0200

    Create Output.hs

---
+++ Sound/FModEx/Raw/Output.hs
@@ -0,0 +1,18 @@
+{- |
+Module      :  Sound.FModEx.Raw.Output
+Description :  Output FModEx library Haskell raw binding
+Copyright   :  (c) Dimitri Sabadie
+License     :  GPL-3
+
+Maintainer  :  dimitri.sabadie@gmail.com
+Stability   :  experimental
+Portability :  Linux only for now
+
+Output FModEx raw Haskell API.
+-}
+
+module Sound.FModEx.Raw.Output (
+    module X
+  ) where
+
+import Sound.FModEx.Raw.Output.Types as X
