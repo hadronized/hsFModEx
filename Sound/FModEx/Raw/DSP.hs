697200cd13599caa4a7452b6632419f725378aa9
Dimitri Sabadie <dimitri.sabadie@me.com>
2013-10-23 14:39:36 +0200

    Create DSP.hs

---
+++ Sound/FModEx/Raw/DSP.hs
@@ -0,0 +1,18 @@
+{- |
+Module      :  Sound.FModEx.Raw.DSP
+Description :  DSP FModEx library Haskell raw binding
+Copyright   :  (c) Dimitri Sabadie
+License     :  GPL-3
+
+Maintainer  :  dimitri.sabadie@gmail.com
+Stability   :  experimental
+Portability :  Linux only for now
+
+DSP FModEx raw Haskell API.
+-}
+
+module Sound.FModEx.Raw.DSP (
+    module X
+  ) where
+
+import Sound.FModEx.Raw.DSP.Types as X
