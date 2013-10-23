4db098fc35696f9127ababb76a3d8ad8c682de9c
Dimitri Sabadie <dimitri.sabadie@me.com>
2013-10-23 14:41:51 +0200

    Create Linux.hs

---
+++ Sound/FModEx/Raw/Linux.hs
@@ -0,0 +1,18 @@
+{- |
+Module      :  Sound.FModEx.Raw.Linux
+Description :  Linux FModEx library Haskell raw binding
+Copyright   :  (c) Dimitri Sabadie
+License     :  GPL-3
+
+Maintainer  :  dimitri.sabadie@gmail.com
+Stability   :  experimental
+Portability :  Linux only for now
+
+Linux FModEx raw Haskell API.
+-}
+
+module Sound.FModEx.Raw.Linux (
+    module X
+  ) where
+
+import Sound.FModEx.Raw.Linux.Types as X
