25a3482c66bbfd1068a2f4a6ce02adf25f01833e
Dimitri Sabadie <dimitri.sabadie@me.com>
2013-10-23 09:38:12 +0200

    Added Codec.hs.

---
+++ Sound/FModEx/Raw/Codec.hs
@@ -0,0 +1,18 @@
+{- |
+Module      :  Sound.FModEx.Raw.Codec
+Description :  Codec FModEx library Haskell raw binding
+Copyright   :  (c) Dimitri Sabadie
+License     :  GPL-3
+
+Maintainer  :  dimitri.sabadie@gmail.com
+Stability   :  experimental
+Portability :  Linux only for now
+
+Codec FModEx raw Haskell API.
+-}
+
+module Sound.FModEx.Raw.Codec (
+    module X
+  ) where
+
+import Sound.FModEx.Raw.Codec.Types as X
